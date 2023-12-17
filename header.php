48d051f75713efe80a80bd6c5b940231b0d47ff1
Andrew Nagi <andrew.nagi@gmail.com>
2023-12-17 20:03:18 +0000

    Remove manifest

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/favicon-16x16.png">
-    <link rel="manifest" href="<?php get_template_directory_uri() ?>/site.webmanifest">
 	<?php
 	wp_head();
 	global $bone_health_job_number;
